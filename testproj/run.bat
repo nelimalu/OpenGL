f223c4c5d336efaa785f8b642e91927533e29412
Luka Jovanovic <lukaj2501@gmail.com>
2023-11-26 21:44:49 -0500

    working shader

--- testproj/run.bat
+++ testproj/run.bat
@@ -1,1 +1,1 @@
-g++ main.cpp -IC:\cpp_libs\glfw\include -LC:\cpp_libs\glfw\lib-vc2022 -lmingw32 -lglfw3dll -lopengl32 -o run && run
+g++ main.cpp -IC:\cpp_libs\glfw\include -LC:\cpp_libs\glfw\lib-vc2022 -IC:\cpp_libs\glew\include -LC:\cpp_libs\glew\lib -lmingw32 -lglfw3dll -lopengl32 -lglew32 -o run && run
